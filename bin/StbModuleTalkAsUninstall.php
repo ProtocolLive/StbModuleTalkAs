5c57fd8a418c8e32567c2b74dc963088a234cc6f
Protocol Corporation <protocollive@outlook.com>
2024-02-14 10:23:05 -0300

    Script de desinstalação

---
+++ bin/StbModuleTalkAsUninstall.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @version 2024.02.14.00
+ */
+
+declare(strict_types = 1);
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '1');
+ini_set('error_reporting', '-1');
+ini_set('html_errors', '0');
+ini_set('max_execution_time', '10');
+ini_set('error_log', __DIR__ . '/error.log');
+
+use ProtocolLive\SimpleTelegramBot\StbObjects\StbAdminModules;
+
+require(dirname(__DIR__, 3) . '/autoload.php');
+
+StbAdminModules::GlobalModuleUninstall('ProtocolLive\\StbModuleTalkAs\\TalkAs');
+echo 'Desinstalação concluída' . PHP_EOL;
